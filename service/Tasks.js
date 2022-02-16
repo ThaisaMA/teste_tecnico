e43be3dee108d8dccf3058d50dcf014142efcf96
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2022-02-16 16:47:34 -0300

    em service/tasks e add funçao getAll

--- service/Tasks.js
+++ service/Tasks.js
@@ -1,9 +1,11 @@
 const { create } = require("../model/Tasks");
 
-const createTasks = async ({ task, status }) => 
-    create({ task, status });
+const createTasks = async ({ task, status }) => create({ task, status });
+
+const getAll = async () => findAll();
 
     
 module.exports = {
     createTasks,
+    getAll,
 }        

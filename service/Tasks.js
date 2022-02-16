a8550dbc76cbf7223d8ed4da880f885b29822a5a
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2022-02-16 16:56:53 -0300

    em service/tasks e add funçao getOne

--- service/Tasks.js
+++ service/Tasks.js
@@ -1,11 +1,19 @@
-const { create, findAll } = require("../model/Tasks");
+const { create, findAll, find } = require("../model/Tasks");
 
 const createTasks = async ({ task, status }) => create({ task, status });
 
 const getAll = async () => findAll();
 
+const getOne = async (id) => {
+    const taskExist = await find(id);
+    if (!taskExist) return { erroCode: 'NOT_EXISTS' };
+    return taskExist;
+};
+
+
     
 module.exports = {
     createTasks,
     getAll,
+    getOne,
 }        

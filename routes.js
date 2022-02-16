e136256f1bbfc2c422579cb985444e932a3465f0
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2022-02-16 17:38:24 -0300

    coloca funçoes nas devidas rotas

--- routes.js
+++ routes.js
@@ -1,9 +1,11 @@
+const { createNewTasks, getAllTasks, getOneTask, updateTaskById, deleteTask } = require('./controller/Tasks');
+
 const router = require('express').Router();
 
-router.post('/',);
-router.get('/',);
-router.get('/:id',);
-router.put('/:id',);
-router.delete('/:id',);
+router.post('/',createNewTasks);
+router.get('/', getAllTasks);
+router.get('/:id', getOneTask);
+router.put('/:id', updateTaskById);
+router.delete('/:id', deleteTask);
 
 module.exports = router;

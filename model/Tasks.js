36de0bf0835a846c14057420516013ccceaeddcc
Thaisa Medeiros <thaisa.ma@hotmail.com.br>
2022-02-16 16:46:21 -0300

    em  model/tasks e add funçao findAll

--- model/Tasks.js
+++ model/Tasks.js
@@ -4,13 +4,20 @@ const collection = 'tasks';
 
 const create = async ({ task, status }) => {
     const db = await connection();
-    const insert = await db.collection(collection).insertOne( 
+    const insertTask = await db.collection(collection).insertOne( 
         { task, status }, 
     );
-    const task = insert.ops[0];
+    const task = insertTask.ops[0];
     return { task };
 };
 
+const findAll = async () => {
+    const db = await connection();
+    const allTasks = await db.collection(collection).find({}).toArray();
+    return allTasks;
+  };
+
 module.exports = {
     create,
+    findAll,
   };
